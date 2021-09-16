bce90357019afe36eab80fd23fe370d56108a987
rialbeg <gsamascarenhas@gmail.com>
2021-09-16 16:56:32 -0300

    colocando on  links e botões funcionando

--- JS/index.js
+++ JS/index.js
@@ -19,15 +19,55 @@
   // }, 1500);
 
 const navbar = document.querySelector("#navbar");
+const navimage = document.querySelector("#logo-nome");
 
 window.onscroll = function(){
     let top = window.scrollY;
     
     if(top >= 100)
     {
-      navbar.classList.add("active");
+      navbar.classList.add("nav-active");
+      navimage.style.width = "20rem";
     }else
     {
-      navbar.classList.remove("active");
+      navbar.classList.remove("nav-active");
+      navimage.style.width = "30rem";
+      navimage.style.transition = "all 0.2s";
     }
 }
+
+/*Smooth Scrolling*/ 
+
+// const links = document.querySelectorAll(".navlist li a");
+
+// for (const link of links) {
+//   link.addEventListener("click", clickHandler);
+// }
+
+// function clickHandler(e) {
+//   e.preventDefault();
+//   const href = this.getAttribute("href");
+//   const offsetTop = document.querySelector(href).offsetTop;
+
+//   scroll({
+//     top: offsetTop,
+//     behavior: "smooth"
+//   });
+// }
+
+
+/*Active link when scrolling*/
+const links = document.querySelectorAll(".navlist li a");
+const sections = document.querySelectorAll('section');
+
+function changeLinkState() {
+  let index = sections.length;
+
+  while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+  
+  links.forEach((link) => link.classList.remove('link-active'));
+  links[index].classList.add('link-active');
+}
+
+changeLinkState();
+window.addEventListener('scroll', changeLinkState);
